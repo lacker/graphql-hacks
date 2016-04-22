e57cf7e1840868dd60ede8585ba6130a999021e2
Kevin Lacker <lacker@gmail.com>
2016-04-21 22:06:09 -0700

    server runs, babels

--- js/index.js
+++ js/index.js
@@ -5,7 +5,10 @@ import {
   GraphQLString
 } from 'graphql';
 
-var schema = new GraphQLSchema({
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+
+const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -19,4 +22,10 @@ var schema = new GraphQLSchema({
   })
 });
 
-console.log('check');
+const app = express();
+
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
+
+app.listen(3000, function () {
+  console.log('server running at http://localhost:3000');
+});
