f2f547ddc3d062172bde2c93aa2b9244cba7d3f9
Kevin Lacker <lacker@gmail.com>
2016-06-11 08:28:14 -0700

    some base62ing

--- grelay/data/ids.js
+++ grelay/data/ids.js
@@ -1,3 +1,35 @@
+import bigInt from 'big-integer';
+
+const CHARMAP = ('0123456789' +
+                 'abcdefghijklmnopqrstuvwxyz' +
+                 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+
+// Converts an int/bigInt into a string using base62 encoding.
+function base62(integer) {
+  integer = bigInt(integer);
+  let chars = [];
+  while (integer.greater(0)) {
+    const { quotient, remainder } = integer.divmod(62);
+    chars.unshift(CHARMAP[remainder.toJSNumber()]);
+    integer = quotient;
+  }
+  return chars.join('');
+}
+
+// The reverse of base62 - converts a string into a bigInt.
+// TODO: test
+function unbase62(string) {
+  let integer = bigInt(0);
+  for (let char of string) {
+    const index = CHARMAP.index(char);
+    if (index < 0) {
+      throw 'bad char: ' + char;
+    }
+    integer = integer.times(62).plus(index);
+  }
+  return integer;
+}
+
 function choice(alist) {
   return alist[Math.floor(Math.random() * alist.length)]
 }
@@ -5,9 +37,7 @@ function choice(alist) {
 function randomString() {
   let answer = '';
   for (let i = 0; i < 10; i++) {
-    answer += choice('0123456789'
-                     + 'abcdefghijklmnopqrstuvwxyz'
-                     + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    answer += choice(CHARMAP);
   }
   return answer;
 }
