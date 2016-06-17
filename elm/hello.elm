cd88e0627ba2c3081f774cd763fd86ca0517da7a
Kevin Lacker <lacker@gmail.com>
2016-06-16 21:04:27 -0700

    fish world

--- elm/hello.elm
+++ elm/hello.elm
@@ -1,3 +1,12 @@
-import Html exposing (text)
+import Html exposing (button, text)
+import Html.App exposing (beginnerProgram)
+import Html.Events exposing (onClick)
 
-main = text "hello elm world"
+main =
+  beginnerProgram { model = "world", view = view, update = update }
+
+view model = button [ onClick "fish" ] [ text (toString model) ]
+
+
+update msg model =
+    msg ++ " " ++ model
