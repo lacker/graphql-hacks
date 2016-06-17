45d133bd4c7c30c0ae60eef0399ac1e1fbee664b
Kevin Lacker <lacker@gmail.com>
2016-06-17 11:31:33 -0700

    expand out to have a div and a list

--- elm/hello.elm
+++ elm/hello.elm
@@ -1,11 +1,14 @@
-import Html exposing (button, text)
+import Html exposing (button, div, text)
 import Html.App exposing (beginnerProgram)
 import Html.Events exposing (onClick)
 
 main =
   beginnerProgram { model = "world", view = view, update = update }
 
-view model = button [ onClick "fish" ] [ text (toString model) ]
+view model = div [] [
+              text "hello",
+              button [ onClick "fish" ] [ text (toString model) ]
+             ]
 
 
 update msg model =
