b780e14b61c0d4452a067e759fccddaee0ee87f6
Kevin Lacker <lacker@gmail.com>
2016-06-19 21:04:10 -0700

    refactor to store history

--- elm/hello.elm
+++ elm/hello.elm
@@ -1,19 +1,25 @@
 import Html exposing (button, br, div, text)
 import Html.App exposing (beginnerProgram)
 import Html.Events exposing (onClick)
+import List exposing (head)
 
 main =
-  beginnerProgram { model = "sometimes", view = view, update = update }
+  beginnerProgram { model = ["sometimes"], view = view, update = update }
 
 mbutton msg = button [ onClick msg ] [ text msg ]
 
 view model = div [] [
-              text (toString model),
+              text (getstring model),
               br [] [],
               mbutton "fish",
               mbutton "buffalo"
              ]
 
+getstring model =
+    case model of
+        [] -> "EMPTY"
+        s :: _ -> s
+
 
 update msg model =
-    model ++ " " ++ msg
+    ((getstring model) ++ " " ++ msg) :: model
