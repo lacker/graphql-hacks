68e9e2487202986d4f4cbaedec4eb2d8ab069c05
Kevin Lacker <lacker@gmail.com>
2016-07-17 21:24:32 -0700

    fix coloring

---
+++ fromscratch/webpack.config.prod.js
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var isInNodeModules = 'node_modules' ===
+  path.basename(path.resolve(path.join(__dirname, '..')));
+var relative = isInNodeModules ? '../..' : '.';
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, relative, 'build'),
+    filename: '[name].[hash].js',
+    // TODO: this wouldn't work for e.g. GH Pages.
+    // Good news: we can infer it from package.json :-)
+    publicPath: '/'
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        include: path.resolve(__dirname, relative, 'src')
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, relative, 'src'),
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'es2016', 'react'],
+          plugins: [
+            'transform-object-rest-spread',
+            'transform-react-constant-elements'
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, relative, 'src'),
+        loader: 'style!css!postcss'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.(jpg|png|gif|eot|svg|ttf|woff|woff2)$/,
+        loader: 'file',
+      },
+      {
+        test: /\.(mp4|webm)$/,
+        loader: 'url?limit=10000'
+      }
+    ]
+  },
+  eslint: {
+    configFile: path.join(__dirname, '.eslintrc')
+  },
+  postcss: function() {
+    return [autoprefixer];
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname, relative, 'index.html'),
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      }
+    }),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true,
+        warnings: false
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+        screw_ie8: true
+      }
+    })
+  ]
+};
