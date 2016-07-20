dfd999fb41ac4c70e0ad21eae2916833d9c8659c
Kevin Lacker <lacker@gmail.com>
2016-07-19 21:27:09 -0700

    scrap fs

--- fromscratch/src/index.js
+++
@@ -1,8 +0,0 @@
-import './index.css';
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
-
-const rootEl = document.createElement('div');
-document.body.appendChild(rootEl);
-render(<App />, rootEl);
