5cde5f3165fed8891a607eef5b50d2045fcebdff
Kevin Lacker <lacker@gmail.com>
2016-07-19 21:27:16 -0700

    unscrap

---
+++ fromscratch/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import logo from './logo.png';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>
+        Welcome to <img src={logo} className="App-logo" alt="logo" /> React
+      </h1>
+      <p>
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
