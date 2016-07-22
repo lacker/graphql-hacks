ed646b6d7d593f85c62046c498f2dba294089858
Kevin Lacker <lacker@gmail.com>
2016-07-21 23:40:49 -0700

    legit flow

--- flowing/dostuff.js
+++ flowing/dostuff.js
@@ -1,16 +1,18 @@
+// @flow
+
 const SortedSet = require('js-sorted-set');
 
 let s = new SortedSet();
 s.insert(1);
 
-function add(x) {
+function add(x: number) {
   if (!s.contains(x)) {
     s.insert(x);
   }
 }
 
 while (true) {
-  let first = s.beginIterator().value();
+  let first: number = s.beginIterator().value();
   console.log(first);
   add(first * 2);
   add(first * 3);
