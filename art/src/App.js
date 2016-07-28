0c62424d9601d1aeb5d0f280f3dbadbb93bc6c35
Kevin Lacker <lacker@gmail.com>
2016-07-27 22:13:16 -0700

    blurring functionality

--- art/src/App.js
+++ art/src/App.js
@@ -1,14 +1,66 @@
 import './App.css';
 import React from 'react';
 
-const PERIHELION = 90;
-const APHELION = 257;
+// Defines the ellipse for Electron and its helper functions.
+const X_RADIUS = 257;
+const Y_RADIUS = 90;
 
+// In the
+function pointOnEllipse(theta) {
+  return {
+    x: Math.cos(theta) * X_RADIUS,
+    y: Math.sin(theta) * Y_RADIUS,
+  };
+}
+
+// props:
+// theta is an angle in radians
+// blur is from 0 = no-blur to 1 = entire-oval
 const Electron = (props) => {
-  let cx = Math.cos(props.theta) * APHELION;
-  let cy = Math.sin(props.theta) * PERIHELION;
+  if (!props.blur) {
+    let {x, y} = pointOnEllipse(props.theta);
+    return (
+      <circle fill="#00D8FF" cx={x} cy={y} r="12" />
+    );
+  }
+
+  if (props.blur > 0.9999) {
+    return (
+      <g>
+        <Electron theta={0} blur={0.6} />
+        <Electron theta={Math.PI} blur={0.6} />
+      </g>
+    );
+  }
+
+  // The arc goes from alpha to beta
+  let alpha = props.theta - (props.blur * Math.PI);
+  let beta = props.theta + (props.blur * Math.PI);
+  let {x: startX, y: startY} = pointOnEllipse(alpha);
+  let {x: endX, y: endY} = pointOnEllipse(beta);
+
+  let largeArc = (props.blur > 0.5) ? 1 : 0;
+
+  let d = [
+    'M',
+    startX,
+    startY,
+    'A',
+    X_RADIUS,
+    Y_RADIUS,
+    0,
+    largeArc,
+    1,
+    endX,
+    endY,
+  ].join(' ');
+
   return (
-    <circle fill="#00D8FF" cx={cx} cy={cy} r="12" />
+    <g>
+      <Electron theta={alpha} />
+      <path d={d} fill="none" stroke="#00D8FF" strokeWidth="24" />
+      <Electron theta={beta} />
+    </g>
   );
 }
 
@@ -30,12 +82,19 @@ export default class App extends React.Component {
   }
 
   onTick() {
-    let elapsed = (new Date()).getTime() - this.start;
-    this.setState({time: elapsed});
+    let time = (new Date()).getTime() - this.start;
+    this.setState({time});
   }
 
   render() {
-    let frac = (this.state.time % 1000) / 1000;
+    // frac cycles from 0 to 1
+    // progress goes monotonically up from 0 to 1
+    let period = 1000;
+    let frac = (this.state.time % period) / period;
+    let startTime = 3000;
+    let finishTime = 6000;
+    let progress = Math.min(1,
+      (Math.max(0, this.state.time - startTime)) / finishTime);
     let theta = (2 * Math.PI) * frac;
     return (
       <div className="container">
@@ -43,12 +102,12 @@ export default class App extends React.Component {
           <rect fill="#222222" width="600" height="600" />
           <g transform="translate(300 300)">
             <circle fill="#00D8FF" cx="0" cy="0" r="50" />
-            <Electron theta={theta} />
+            <Electron theta={theta} blur={progress} />
             <g transform="rotate(120)">
-              <Electron theta={theta} />
+              <Electron theta={theta} blur={progress} />
             </g>
             <g transform="rotate(240)">
-              <Electron theta={theta} />
+              <Electron theta={theta} blur={progress} />
             </g>
           </g>
         </svg>
