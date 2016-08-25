512ebbdd3ce7d61648c856369868ebc9ab7cde62
Kevin Lacker <lacker@gmail.com>
2016-08-24 22:53:37 -0700

    more mexampley stuff

---
+++ mexample/database.js
@@ -0,0 +1,9 @@
+const mongodb = require('mongodb-prebuilt');
+
+// Start mongo.
+// In a real production environment you wouldn't want your application
+// server to start your database process like this.
+const MONGO_PORT = 1337;
+mongodb.start_server({args: {port: MONGO_PORT}});
+
+// TODO: export a client
--- mexample/server.js
+++ mexample/server.js
@@ -1,7 +1,37 @@
-const mongodb = require('mongodb-prebuilt');
+// TODO: use require so that it works in straight node
+import buildSchema from 'graphql';
+import graphqlHTTP from 'express-graphql';
 
-// Start mongo.
-// In a real production environment you wouldn't want your application
-// server to start your database process like this.
-const MONGO_PORT = 1337;
-mongodb.start_server({args: {port: MONGO_PORT}});
+const app = express();
+
+// TODO: more features besides login + adding a todo
+const schema = buildSchema(`
+  type Todo {
+    id: String
+    text: String
+    completed: Boolean
+  }
+
+  type User {
+    username: String
+    todos: [Todo]
+    authToken: String
+  }
+
+  type Query {
+    me: User
+    login(username: String, password: String): User
+  }
+
+  type Mutation {
+    signup(username: String, password: String): User
+    addTodo(text: String): Todo
+  }
+`);
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+app.listen(3000);
