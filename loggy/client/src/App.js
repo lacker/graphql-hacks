260551f291c6e6b646108f1d7aab08ee2ce617f4
Kevin Lacker <lacker@gmail.com>
2016-09-08 21:21:22 -0700

    UsernamePasswordForm

--- loggy/client/src/App.js
+++ loggy/client/src/App.js
@@ -1,33 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class Content extends Component {
+class UsernamePasswordForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onSubmit = () => {
+      props.onSubmit(this.state.username, this.state.password);
+    };
+
+    this.state = {
+      username: '',
+      password: '',
+    };
+
+    this.onUsernameChange = this.onUsernameChange.bind(this)
+    this.onPasswordChange = this.onPasswordChange.bind(this)
+  }
+
+  onUsernameChange(event) {
+    this.setState({username: event.target.value})
+  }
+
+  onPasswordChange(password) {
+    this.setState({password: event.target.value})
+  }
+
   render() {
-    if (this.props.username) {
-      return (
-        <p>logged in as: {this.props.username}</p>
-      );
-    } else {
-      return (
-        <p>you need to log in!</p>
-      );
-    }
+    return (
+      <form onSubmit={this.onSubmit}>
+        username:
+        <br />
+        <input
+          type='text'
+          onChange={this.onUsernameChange}
+          value={this.state.username} />
+        <br />
+        password:
+        <br />
+        <input
+          type='password'
+          onChange={this.onPasswordChange}
+          value={this.state.password} />
+        <br /><br />
+        <input type='submit' value={this.props.value} />
+      </form>
+    );
   }
 }
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+    if (!this.state.username) {
+      return (
+        <div>
+          <p>sign up with a new account:</p>
+          <UsernamePasswordForm onSubmit={'TODO'} value='sign up' />
+          <p>log in with an existing account:</p>
+          <UsernamePasswordForm onSubmit={'TODO'} value='log in' />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      );
+    }
+    return (
+      <p>you are logged in as {this.state.username}</p>
     );
   }
 }
