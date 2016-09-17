0dac535c61e83e2f689325906e3eaec8509784a5
Kevin Lacker <lacker@gmail.com>
2016-09-16 22:42:39 -0700

    set appropriate error code

--- mexample/mongo.js
+++ mexample/mongo.js
@@ -4,7 +4,9 @@ var { MongoClient } = require('mongodb');
 // Set up the database.
 // In a real production environment you wouldn't want your application
 // server to start your database process like this.
-prebuilt.start_server();
+prebuilt.start_server(null, (errorCode) => {
+  console.log('got error code', errorCode);
+});
 
 // Connect to the 'todofullstack' database on the default Mongo port.
 // Returns a promise that resolves once we have connected to the database.
