bf9352230e0fc0603594aa5ac85d993eddce2772
Kevin Lacker <lacker@gmail.com>
2016-09-20 22:16:03 -0700

    add todos to database.js

--- relayhello/data/database.js
+++ relayhello/data/database.js
@@ -10,6 +10,7 @@
 // Model types
 class User {}
 class Widget {}
+class Todo {}
 
 // Mock data
 var viewer = new User();
@@ -22,12 +23,16 @@ var widgets = ['What\'s-it', 'Who\'s-it', 'How\'s-it'].map((name, i) => {
   return widget;
 });
 
+var todos = ['learn relay', 'make better quickstart', 'profit'];
+
 module.exports = {
   // Export methods that your schema can use to interact with your database
   getUser: (id) => id === viewer.id ? viewer : null,
   getViewer: () => viewer,
   getWidget: (id) => widgets.find(w => w.id === id),
   getWidgets: () => widgets,
+  getTodo: (id) => todos[id],
   User,
   Widget,
+  Todo,
 };
